6f9d5d197c56c52987d537767a1e59914f8f95d0
Luke Langford <luke.david.langford@gmail.com>
2018-06-16 16:09:27 +1000

    GUI & Refactoring
    Added simple GUI to display weather information.
    Refactored code.
    
    Added functionality to reload weather data every 5 seconds to keep it realtime.

--- WeatherApp/src/main/java/com/langford/maven/WeatherApp/App.java
+++ WeatherApp/src/main/java/com/langford/maven/WeatherApp/App.java
@@ -1,18 +1,73 @@
 package com.langford.maven.WeatherApp;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.*;
+
 public class App {
+	
+	private Weather weather = new Weather();
+	private JFrame frame;
+	private JPanel panel;
 	
     public static void main(String[] args) {
     	
-        Weather weather = new Weather();
-        weather.loadWeatherData();
+    	App app = new App();
+    	app.loadWeatherData();
+    	app.setupUI();
+    	app.loadUI();
+    	app.updateUI();
+    	
         
-        System.out.println("Location: " + weather.getLocationName());
-        System.out.println("Description: " + weather.getDescription());
-        System.out.println("Temperature: " + weather.getTemp());
-        System.out.println("Humidity: " + weather.getHumidity() + "%");
-        System.out.println("Cloud Coverage: " + weather.getCloudCov() + "%");
+    }
+    
+    private void loadWeatherData() {
+    	weather.loadWeatherData();
+    }
+    
+    private void setupUI() {
+    	
+    	frame = new JFrame("Current Weather");
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+    	
+    }
+    
+    private void loadUI() {
+        
+    	panel.removeAll();
+        JLabel locLabel = new JLabel("Location: " + weather.getLocationName());
+        JLabel descLabel = new JLabel("Description: " + weather.getDescription());
+        JLabel tempLabel = new JLabel(String.format("Temperature: %.2f C", weather.getTemp()));
+        JLabel humdLabel = new JLabel("Humidity: " + weather.getHumidity() + "%");
+        JLabel cloudLabel = new JLabel("Cloud Coverage: " + weather.getCloudCov() + "%");
+        
+        panel.add(locLabel);
+        panel.add(descLabel);
+        panel.add(tempLabel);
+        panel.add(humdLabel);
+        panel.add(cloudLabel);
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+    	
+    }
+    
+    private void updateUI() {
+    	Runnable helloRunnable = new Runnable() {
+	        public void run() {
+	        	loadWeatherData();
+	            loadUI();
+	        }
+	    };
+	
+	    ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+	    executor.scheduleAtFixedRate(helloRunnable, 0, 3, TimeUnit.SECONDS);
+		
     }
     
 }
